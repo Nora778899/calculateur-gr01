14cf68eb056d24983d2c8f79c216e03caab384ee
Nora778899 <nora.boukal@icloud.com>
2026-05-26 14:49:55 +0200

    Ajout

--- calculateur.py
+++ calculateur.py
@@ -1,6 +1,15 @@
-def calculer_tva(prix_ht: float) -> float:
+ddef calculer_tva(prix_ht: float) -> float:
     return prix_ht * 1.20
 
 
-    prix_ttc = calculer_tva(100)
-    print(f"Total TTC: {prix_ttc}")
+
+def calculer_remise(prix: float, remise_pct: float) -> float:
+    """Applique une remise en pourcentage sur un prix.
+
+    Exemple : calculer_remise(100, 20) -> 80.0
+    """
+    return prix * (1 - remise_pct/100)
+    
+
+prix_ttc = calculer_tva(100)
+print(f"Total TTC: {prix_ttc}") 
